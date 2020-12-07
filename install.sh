7f5e98739b945c34d56254c77253b7e6b66b5323
gabrielsebag <gabriel.sebag@efrei.net>
2020-12-07 10:32:49 +0100

    Updated docker compose version

--- install.sh
+++ install.sh
@@ -18,6 +18,8 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt install -y docker-ce
 sudo apt install -y docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 # DOCKER
 # ------------------------------------------------------------------------------
