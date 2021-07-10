74d240944054f3a6fdbd39726ba9144fd067a254
Gabriel Sebag <gabriel.sebag@efrei.net>
2021-07-10 11:12:30 +0200

    Install nodejs as sudo

--- install.sh
+++ install.sh
@@ -28,5 +28,5 @@ sudo apt install -y make
 # NODE
 # ------------------------------------------------------------------------------
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt update && \
-  apt install -y nodejs
+sudo apt update
+sudo apt install -y nodejs
