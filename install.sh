d809917b4e49baca39079912bc57334bd48f8349
Gabriel Sebag <gabriel.sebag@efrei.net>
2022-06-21 19:29:00 +0200

    Updated script

--- install.sh
+++ install.sh
@@ -2,31 +2,30 @@
 
 # DOCKER
 # ------------------------------------------------------------------------------
-# apt-get remove docker docker-engine docker.io
-sudo apt update
-sudo apt install -y \
+sudo apt-get update -y
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-sudo apt update
-sudo apt install -y docker-ce
-sudo apt install -y docker-compose
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 
-# DOCKER
+# MAKE
 # ------------------------------------------------------------------------------
-sudo apt install -y make
+sudo apt-get install -y make
 
 # NODE
 # ------------------------------------------------------------------------------
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt update
-sudo apt install -y nodejs
+sudo curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get update -y
+sudo apt-get install -y nodejs
